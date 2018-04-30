9150255ca0c3f70ab20481025fec8aeca1985b7f
guilhermengler <g.eengler@gmail.com>
2018-04-30 09:44:35 -0300

    change version

--- Formula/unison.rb
+++ Formula/unison.rb
@@ -6,7 +6,7 @@ class Unison < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8b037bddb50c9b0337782da399c5419b9c06b7aa955ecfb9edfcabfc902bdfb2" => :high_sierra
+    sha256 "1622a673b2bc8ef6d00dd754c26cfd3d5f737ed1dc48a46bbce486fa0f766181" => :high_sierra
     sha256 "fd00605490100ec224f5d86a79da341ce639e1d0772fb48cc133672d1bc2a812" => :sierra
     sha256 "53c945e143961a5d158a36980664d5be7153b84074bfa4220eee2db35404b7d3" => :el_capitan
   end
